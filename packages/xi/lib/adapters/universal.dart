1d1dddf0bb161a5152671660d6c0bc482897d370
d1y <chenhonzhou@gmail.com>
2025-09-11 01:55:09 +0800

    add js2 demo

--- packages/xi/lib/adapters/universal.dart
+++ packages/xi/lib/adapters/universal.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 import 'package:dart_qjson/dart_qjson.dart';
 import 'package:xi/xi.dart';
@@ -9,9 +11,34 @@ extension BetterJSONList on JsonList {
       cb(cx);
     }
   }
+
+  List<T> map<T>(T Function(JsonObject value) cb) {
+    List<T> result = [];
+    forEach((item) {
+      result.add(cb(item));
+    });
+    return result;
+  }
 }
 
 class UniversalSpider extends ISpiderAdapter {
+  var url = "https://chaojisousuo14.buzz";
+  String _generateJSCode(String realCode, {Map<String, dynamic>? params}) {
+    var ps = jsonEncode(params ?? {});
+    var result = """
+(async ()=> {
+  const env = {
+    get(key, defaultValue) {
+      return this.params[key] ?? defaultValue
+    },
+    baseUrl: `$url`,
+    params: $ps,
+  };
+  $realCode
+})()""";
+    return result;
+  }
+
   List<SourceSpiderQueryCategory> parseCategoryWithJSResult(String _result) {
     var jsonList = JsonList.fromJsonString(_result);
     List<SourceSpiderQueryCategory> result = [];
@@ -30,11 +57,31 @@ class UniversalSpider extends ISpiderAdapter {
       var cover = item.get("cover").toString();
       var title = item.get("title").toString();
       var id = item.get("id").toString();
+      var remark = item.get("remark").toString();
+      var playlist = item.getList("playlist");
+      List<Videos> realVideos = [];
+      if (playlist != null && playlist.isNotEmpty) {
+        var videoInfos = playlist.map((item) {
+          return VideoInfo(
+            name: item.get("text").toString(),
+            url: item.get("id").toString(),
+            type: VideoType.iframe,
+          );
+        });
+        realVideos.add(
+          Videos(
+            title: "默认",
+            datas: videoInfos,
+          ),
+        );
+      }
       result.add(
         VideoDetail(
           id: id,
           title: title,
+          remark: remark,
           extra: {},
+          videos: realVideos,
           smallCoverImage: cover,
         ),
       );
@@ -46,42 +93,14 @@ class UniversalSpider extends ISpiderAdapter {
   Future<List<SourceSpiderQueryCategory>> getCategory() async {
     var result = await js2.evalSync(r"""
 (async ()=> {
-  const url = "https://www.laodifang.tv";
-  const text = await (await fetch(url)).text();
-  const $ = cheerio.load(text);
-  const result = $(".top_nav.clearfix").find("li").toArray().map(el=> {
-    const a = $(el).find('a')
-    const text = a.text()
-    let id = a.attr("href") ?? "-1"
-    id = id.replace("/vodtype/", "").replace(".html", "")
-    return { text, id }
-  })
-  return result
+  return [
+    { text: "亚洲性爱", id: "33" },
+    { text: "热门事件", id: "37" },
+    { text: "动漫肉番", id: "34" },
+  ]
 })()
 """, timeout: Duration(seconds: 3));
     return parseCategoryWithJSResult(result);
-  }
-
-  @override
-  Future<VideoDetail> getDetail(String movieId) async {
-    var result = await js2.evalSync(r"""
-(async ()=> {
-    const url = "https://www.laodifang.tv/voddetail/$$id"
-    const text = await (await fetch(url)).text();
-    const $ = cheerio.load(text);
-    const ctx = $(".content_box.clearfix")
-    const a = ctx.find(".vodlist_thumb")
-    const cover = a.attr('data-original')
-    const title = a.attr('title')
-    const playlist = $("#playlistbox ul li").toArray().map(el=> {
-      const a = $(el).find('a')
-      return { text: a.text(), id: a.attr("href") ?? "-1" }
-    })
-    return [{ cover, title, playlist, id: `$$id` }]
-})()
-"""
-        .replaceAll(r"$$id", movieId), timeout: Duration(seconds: 3));
-    return parseListWithJSResult(result)[0];
   }
 
   @override
@@ -90,24 +109,55 @@ class UniversalSpider extends ISpiderAdapter {
     int limit = 10,
     String? category,
   }) async {
-    if (category == "" || category == "/") return [];
-    var result = await js2.evalSync(r"""
-(async () => {
-  const url = "https://www.laodifang.tv/$$url";
-  const text = await (await fetch(url)).text()
-  const $ = cheerio.load(text);
-  const result = $(".vodlist.vodlist_wi.clearfix").find('li.vodlist_item').toArray().map(el=> {
-    const a = $(el).find('a.vodlist_thumb')
-    const cover = a.attr('data-original')
-    const title = a.attr('title')
-    const id = (a.attr('href') ?? "").replace('/voddetail/', '')
-    return { id, cover, title }
-  })
-  return result
-})()
-"""
-        .replaceAll(r"$$url", "/vodtype/${category!}-$page.html"), timeout: Duration(seconds: 3));
+    var result = await js2.evalSync(_generateJSCode(r"""
+const url = `${env.baseUrl}/index.php/vod/type/id/${env.get('category')}/page/${env.get('page')}.html`;
+const html = await req(url)
+const $ = cheerio.load(html)
+return $(".vodlist .listpic").toArray().map(el=> {
+  const a = $(el).find("a")
+  const pic = $(el).find(".vodpic")
+  const name = $(el).find(".vodname")
+  const remark = $(el).find(".time")
+  return {
+    id: a.attr("href") ?? "",
+    cover: pic.attr("data-original") ?? "",
+    title: name.text() ?? "",
+    remark: remark.text()
+  }
+})
+""", params: {
+      "category": category,
+      "page": page,
+      "limit": limit,
+    }));
     return parseListWithJSResult(result);
+  }
+
+  @override
+  Future<VideoDetail> getDetail(String movieId) async {
+    var result = await js2.evalSync(
+        _generateJSCode(r"""
+const id = env.get("movieId")
+const html = await req(`${env.baseUrl}${id}`)
+const $ = cheerio.load(html)
+const img = $(".pull-left.pull-left-mobile1 img.lazy")
+const playlist = $("#playlist4 tr").toArray().map((item)=> {
+  const a = $(item).find("a")
+  const id = a.attr("href")
+  const text = a.text()
+  return { text, id }
+})
+return [{
+  title: img.attr("title") ?? "",
+  cover: img.attr("src") ?? "",
+  id: id,
+  playlist,
+}]
+""", params: {
+          "movieId": movieId,
+        }),
+        timeout: Duration(seconds: 3));
+    return parseListWithJSResult(result)[0];
   }
 
   @override
@@ -116,23 +166,25 @@ class UniversalSpider extends ISpiderAdapter {
     int page = 1,
     int limit = 10,
   }) async {
-    js2.evalSync(r"""
-(async ()=> {
-  const url = "https://www.laodifang.tv/vodsearch/-------------.html?wd=$$wd&submit=";
-  const text = await (await fetch(url)).text();
-  const $ = cheerio.load(text);
-  const result = $('.vodlist.clearfix').find('li.searchlist_item').toArray().map(el=> {
-    const a = $(el).find('a.vodlist_thumb')
-    const cover = a.attr('data-original')
-    const title = a.attr('title')
-    const id = (a.attr('href') ?? "").replace('/voddetail/', '')
-    return { id, cover, title }
-  })
-  return result
-})()
-"""
-        .replaceAll(r"$$wd", keyword), timeout: Duration(seconds: 3));
-    return [];
+    var result = await js2.evalSync(_generateJSCode(r"""
+const url = `${env.baseUrl}/index.php/vod/search/page/${env.get("page")}/wd/${env.get("keyword")}.html`
+const html = await (await fetch(url)).text()
+const $ = cheerio.load(html)
+return $(".show-list li").toArray().map(item=> {
+  const _ = $(item).find("img") ?? ""
+  const a = $(item).find("a.play-img")
+  const remark = $($(item).find("dl.fn-left").toArray().at(-1)).find("dd").text()
+  const id = a.attr("href") ?? ""
+  const cover = _.attr("src") ?? ""
+  const title = _.attr("alt") ?? ""
+  return { id, cover, title, remark }
+})
+""", params: {
+      "page": page,
+      "limit": limit,
+      "keyword": keyword,
+    }));
+    return parseListWithJSResult(result);
   }
 
   @override
@@ -147,6 +199,16 @@ class UniversalSpider extends ISpiderAdapter {
 
   @override
   Future<List<String>> parseIframe(String iframe) async {
-    return [];
+    var result = await js2.evalSync(_generateJSCode(r"""
+const url = `${env.baseUrl}${env.get('iframe')}`
+const html = await req(url)
+const $ = cheerio.load(html)
+const script = $("#bofang_box script").text()
+const m3u8 = script.match(/"url":"(.*?)"/)[1].replace(/\\/g, '')
+return m3u8
+""", params: {
+      "iframe": iframe,
+    }));
+    return [result];
   }
 }
--- packages/xi/lib/utils/js2.dart
+++ packages/xi/lib/utils/js2.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_js/extensions/fetch.dart';
 import 'package:flutter_js/extensions/xhr.dart';
 import 'package:flutter_js/flutter_js.dart';
+import 'package:xi/xi.dart';
 
 class JS2 {
   late JavascriptRuntime _runtime;
@@ -11,7 +12,21 @@ class JS2 {
     await _runtime.enableHandlePromises();
     await _runtime.enableFetch();
     await _installCheerio();
+    _runtime.setInspectable(true);
     _runtime.enableXhr();
+    _injectMethods();
+  }
+
+  void _injectMethods() {
+    _runtime.injectMethod('req', (dynamic args) async {
+      if (args is List) {
+        var url = args[0];
+        var resp = await XHttp.dio.get(url);
+        var body = resp.data.toString();
+        return body;
+      }
+      return "NULL";
+    });
   }
 
   Future<void> _installCheerio() async {
